f167529f9cfa5bb89e7b3370277e8409bfcd3484
RafaelMQs <rafaeelmq08@gmail.com>
2021-11-03 17:49:08 -0300

    Ajustando DAO e Controller

--- src/main/java/br/com/conexao/conexao/controller/UsuarioController.java
+++ src/main/java/br/com/conexao/conexao/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package br.com.conexao.conexao.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,8 +17,14 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioDAO dao;
 	
+	@GetMapping("/Usuario")
 	public ResponseEntity<List<Usuario>> getAll(){
 		List<Usuario> list = (List<Usuario>)dao.findAll();
+	
+		if(list.size()==0) {
+			return ResponseEntity.status(404).build();
+		}
+		
 		return ResponseEntity.ok(list);
 	}
 	
