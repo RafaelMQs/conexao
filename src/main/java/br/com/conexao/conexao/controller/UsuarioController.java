da9efa48a0e0f6a6019f986a2d62075efb4f6ad0
RafaelMQs <rafaeelmq08@gmail.com>
2021-11-05 17:45:06 -0300

    Adicionando busca por ID

--- src/main/java/br/com/conexao/conexao/controller/UsuarioController.java
+++ src/main/java/br/com/conexao/conexao/controller/UsuarioController.java
@@ -4,9 +4,11 @@ package br.com.conexao.conexao.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 import br.com.conexao.conexao.beans.Usuario;
 import br.com.conexao.conexao.dao.UsuarioDAO;
@@ -17,16 +19,21 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioDAO dao;
 	
-	@GetMapping("/Usuario")
+	@GetMapping("/usuarios")
 	public ResponseEntity<List<Usuario>> getAll(){
 		List<Usuario> list = (List<Usuario>)dao.findAll();
-	
-		if(list.size()==0) {
-			return ResponseEntity.status(404).build();
-		}
+		
+		if(list.size()==0) { return ResponseEntity.status(404).build(); }
+		
 		
 		return ResponseEntity.ok(list);
 	}
 	
+	@GetMapping("/usuario/{userId}")
+	public ResponseEntity<Optional<Usuario>> getAllByID(@PathVariable("userId") int id){
+		Optional<Usuario> list = dao.findById(id);
+		return ResponseEntity.ok(list);
+	}
 	
+
 }
